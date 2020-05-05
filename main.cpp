89e2c910473a6872c263bc7b309ec9f18674fa29
marcusmariette <mariettemarcus@gmail.com>
2020-05-05 16:41:39 +1000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    for (int i = 0; i < argc; i++) 
+    {
+        cout << "Hello World" << endl;
+    }
+ 
+    return 0;
+}
